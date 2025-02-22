89234842cd499aabdab1a8bd798565cd0980f895
Ines Ochoa <ines.ochoa@cern.ch>
2025-02-22 22:12:02 +0000

    exercise 3

---
+++ ftag_dataset/get_data.sh
@@ -0,0 +1,8 @@
+wget https://zenodo.org/records/4044628/files/test_data.root?download=1
+mv test_data.root?download=1 test_data.root
+
+wget https://zenodo.org/records/4044628/files/training_data.root?download=1
+mv training_data.root?download=1 training_data.root
+
+wget https://zenodo.org/records/4044628/files/valid_data.root?download=1
+mv valid_data.root?download=1 valid_data.root
---
+++ ftag_dataset/get_dataloaders.sh
@@ -0,0 +1,5 @@
+wget https://cernbox.cern.ch/s/URJeVkP0lndI7lg/download -O val_dl.pth
+
+wget https://cernbox.cern.ch/s/ivafCy8wbv3Ozhk/download -O test_dl.pth
+
+wget https://cernbox.cern.ch/s/zclKISp42H4kJIX/download -O train_dl.pth
